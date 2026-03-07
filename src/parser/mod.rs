6def103f9b3effb3c2cd4909e6459a7b6be6b151
Jan Klaas Kollhof <jan@kollhof.net>
2026-03-07 09:29:48 +0000

    Remove dead BlockComment token kind

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -87,7 +87,7 @@ impl<'src> Parser<'src> {
   }
 
   fn skip_trivia(&mut self) {
-    while matches!(self.current.kind, TokenKind::Comment | TokenKind::BlockComment | TokenKind::CommentStart | TokenKind::CommentText | TokenKind::CommentEnd) {
+    while matches!(self.current.kind, TokenKind::Comment | TokenKind::CommentStart | TokenKind::CommentText | TokenKind::CommentEnd) {
       self.current = self.lexer.next_token();
     }
   }
